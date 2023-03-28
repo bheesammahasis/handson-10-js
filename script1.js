b96364086a948cb4439f98a6cbcf9a74591013c6
its om <bheesammahasis108@gmail.com>
2023-03-28 14:47:58 +0530

    om12

--- script1.js
+++ script1.js
@@ -1,25 +1,25 @@
  //answer 10
-      let checkNumber = document.querySelector('input');
+      let checkNumber = document.querySelectorAll('input');
       let btn = document.querySelector('#b2');
       let para = document.querySelectorAll('p');
       let randomNumber = Math.floor(Math.random()*100+1);
       let count=100;
      btn.addEventListener('click',() => {
      console.log(randomNumber);
-     console.log(para);
-     if(randomNumber < parseInt(checkNumber.value)){
+     console.log(checkNumber[1].value);
+     if(randomNumber < parseInt(checkNumber[1].value)){
          para[0].innerText = "your guess is higher";
          console.log(para[0])
          count--;
          para[2].innerText=count;
      }
-     else if(randomNumber < parseInt(checkNumber.value)){
+     else if(randomNumber > parseInt(checkNumber[1].value)){
          para[0].innerText = "your guess is lower";
          console.log(para[0])
          count--;
          para[2].innerText=count;
      }
-     else if(randomNumber === parseInt(checkNumber.value)){
+     else if(randomNumber === parseInt(checkNumber[1].value)){
         para[0].innerText = '🤩🤩 Hurray You Won 🤩🤩';
         count--;
         para[2].innerText=count;
